d0954b88f44a899d81383735775d18719824fe50
Madiha <madeeha.hiba123@gmail.com>
2024-12-18 18:27:49 +0500

    User login authentication changes

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,8 +1,37 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const userContext = createContext()
 const authContext = ({children}) => {
     const [user, setUser] = useState(null)
+    const[loading, setLoading] = useState(true)
+
+    useEffect(() =>{
+        const verifyUser = async() =>{
+            try{
+                const token = localStorage.getItem('token')
+                if (token){
+                    const response = await axios.get('http://localhost:5000/api/auth/verify', {
+                        headers:{
+                            "Authorization" :`Bearer ${token}`
+                        }
+                    })
+                    if(response.data.success){
+                        setUser(response.data.user)
+                    }
+                } else {
+                    setUser(null)
+                }
+            } catch(error){
+                if(error.response && !error.response.data.error){
+                    setUser(null)
+                }
+            } finally {
+                setLoading(false)
+            }
+        }
+        verifyUser()
+    }, [])
 
     const login = (user) => {
         setUser(user)
@@ -13,7 +42,7 @@ const authContext = ({children}) => {
         localStorage.removeItem("token")
     }
     return(
-        <userContext.Provider value = {{user, login, logout}}>
+        <userContext.Provider value = {{user, login, logout, loading}}>
             {children}
         </userContext.Provider>
     )
